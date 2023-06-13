0ef55f94979b002d8a5f2c7a84c898b493e7a926
0xmichael <798906172@qq.com>
2023-06-13 18:17:16 +0800

    detail structure

--- zhdaily/src/views/Detail.jsx
+++ zhdaily/src/views/Detail.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
+import './Detail.less';
+import { LeftOutline, MessageOutline, LikeOutline, StarOutline, MoreOutline } from 'antd-mobile-icons';
+import { Badge } from 'antd-mobile';
 
 const Detail = function Detail() {
-  return <div className="detail-box">详情页</div>;
+  return (
+    <div className="detail-box">
+      {/* 底部栏 */}
+      <div className="tab-bar">
+        {/* 返回 */}
+        <div className="back">
+          <LeftOutline />
+        </div>
+
+        <div className="icons">
+          {/* 评论 */}
+          <Badge content={'128'}>
+            <MessageOutline />
+          </Badge>
+
+          {/* 点赞 */}
+          <Badge content="29">
+            <LikeOutline />
+          </Badge>
+
+          {/* 收藏 */}
+          <span className="stored">
+            <StarOutline />
+          </span>
+
+          {/* 更多 */}
+          <span>
+            <MoreOutline />
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Detail;
